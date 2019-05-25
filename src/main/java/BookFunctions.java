fb6f4100a0907ac875c9107c3a0b8b1b59ebca0d
Komputer Domowy <bartosz9-86@tlen.pl>
2019-05-25 14:35:58 +0200

    zadania

--- src/main/java/BookFunctions.java
+++ src/main/java/BookFunctions.java
@@ -1,13 +1,100 @@
 import java.util.List;
+import java.util.Optional;
+import java.util.Comparator;
 
-public class BookFunctions {
-    //Stream
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.collect;
+
+public class BookFunctions extends Book {
+
+    public BookFunctions(String title, String year, String ISBN) {
+        super(title, year, ISBN);
+    }
+
     //zad 1
-    public static Book findBook(String ISBN, List<Book> bookList){
-        Book book = null;
-        bookList.stream()
-                .filter(book1 ->(book.getISBN().equals(ISBN)));
+    public static Book findBook(String ISBN, List<Book> bookList) {
 
+        for (Book book : bookList) {
+            if (book.getISBN().equals(ISBN)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    //zad1 stream
+    public static Optional<Book> findBookStream(String ISBN, List<Book> bookList) {
+
+        Optional<Book> book = bookList.stream()
+                .filter(book1 -> book1.getISBN().equals(ISBN))
+                .findFirst();
+        return book;
+    }
+
+    //zad 2
+    public static List<Book> findTwoLastBook(List<Book> bookList) {
+        List<Book> sub = bookList.subList(bookList.size() - 2, bookList.size() - 1);
+        return sub;
+    }
+
+    //zad2 Stream
+//    public static List<Book> findTwoLastBookStream(List<Book>bookList){
+//        List<Book> listBook = bookList.stream()
+//                .filter(e->bookList.subList(bookList.size()-1,bookList.size()-2))  // FK! NIE WIEM JAK TO Zrobic
+//                .collect(Collectors.toList());
+//
+//        return listBook;
+//    }
+//
+    //zad 3
+    public static Book minYearBook(List<Book> bookList) {
+        Book book;
+        int ind = -1;
+        int minyear = 9999;
+        for (int i = 0; i < bookList.size(); i++) {
+            int year = Integer.parseInt(bookList.get(i).getYear());
+            if (year < minyear) {
+                ind = i;
+                minyear = year;
+            }
+        }
+        book = bookList.get(ind);
+        return book;
+
+    }
+
+    //zad 3 stream
+    public static Optional<Book> minYearBookStream(List<Book> bookList) {
+
+
+        Optional<Book> book = bookList.stream()
+                .min(Comparator.comparing(Book::getYear));
+        return book;
+    }
+
+    //zad 4
+    public static Book maxYearBook(List<Book> bookList) {
+        Book book;
+        int ind = -1;
+        int maxyear = 0;
+        for (int i = 0; i < bookList.size(); i++) {
+            int year = Integer.parseInt(bookList.get(i).getYear());
+            if (year > maxyear) {
+                ind = i;
+                maxyear = year;
+            }
+        }
+        book = bookList.get(ind);
+        return book;
+
+    }
+    //zad 4 stream
+    public static Optional<Book> maxYearBookStream(List<Book> bookList) {
+
+
+        Optional<Book> book = bookList.stream()
+                .max(Comparator.comparing(Book::getYear));
         return book;
     }
 }
+
+
--- src/main/java/BookFunctionsTest.java
+++ src/main/java/BookFunctionsTest.java
@@ -1,39 +1,79 @@
 import org.junit.Assert;
 import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class BookFunctionsTest {
-//    List<Book> bookList = new ArrayList<>();
-//
-//    Book book1 = new Book("Pierwsza", "1999", "12345");
-//    Book book2 = new Book("Druga", "2001", "1256");
-//    Book book3 = new Book("Trzecia", "2005", "2564");
-//    Book book4 = new Book("Czwarta", "2010", "1258");
-//    Book book5 = new Book("Piąta", "1986", "125687");
-//    Book book6 = new Book("Szósta", "2017", "12547");
-//
-//    public void addBooks() {
-//        bookList.add(book1);
-//        bookList.add(book2);
-//        bookList.add(book3);
-//        bookList.add(book4);
-//        bookList.add(book5);
-//        bookList.add(book6);
-//    }
-//
-//    @Test
-//    public void findISBN() {
-//        bookList.add(book1);
-//        bookList.add(book2);
-//        bookList.add(book3);
-//        bookList.add(book4);
-//        bookList.add(book5);
-//        bookList.add(book6);
-//        Book book = BookFunctions.findBook("12345",bookList);
-//        Assert.assertEquals("12345", book.getISBN());
-//    }
-//
-//}
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class BookFunctionsTest {
+    List<Book> bookListTest = new ArrayList<>();
+
+    @Before
+    public void addBooks() {
+
+        Book book1 = new Book("Pierwsza", "1999", "12345");
+        Book book2 = new Book("Druga", "2001", "1256");
+        Book book3 = new Book("Trzecia", "2005", "2564");
+        Book book4 = new Book("Czwarta", "2010", "1258");
+        Book book5 = new Book("Piąta", "1986", "125687");
+        Book book6 = new Book("Szósta", "2017", "12547");
+
+        bookListTest.add(book1);
+        bookListTest.add(book2);
+        bookListTest.add(book3);
+        bookListTest.add(book4);
+        bookListTest.add(book5);
+        bookListTest.add(book6);
+    }
+
+    //zad 1 for
+    @Test
+    public void findISBN() {
+        Book book = BookFunctions.findBook("12345", bookListTest);
+        Assert.assertEquals("Pierwsza", book.getTitle());
+    }
+
+    //zad 1 streeam
+    @Test
+    public void findISBNStream() {
+        Optional<Book> book = BookFunctions.findBookStream("12345", bookListTest);
+        Assert.assertEquals("Pierwsza", book.get().getTitle());
+    }
+
+    //zad 2
+    @Test
+    public void findTwoLastBook() {
+        List<Book> sub = BookFunctions.findTwoLastBook(bookListTest);
+        Assert.assertEquals(bookListTest.subList(4, 5), sub);
+    }
+    //zad2 Stream
+    //
+
+    //zad3
+    @Test
+    public void findMinYearBook() {
+        Book book = BookFunctions.minYearBook(bookListTest);
+        Assert.assertEquals("Piąta", book.getTitle());
+    }
+
+    //zad 3 stream
+    @Test
+    public void findMinYearBookStream() {
+        Optional<Book> book = BookFunctions.minYearBookStream(bookListTest);
+        Assert.assertEquals("Piąta", book.get().getTitle());
+    }
+    //zad 4
+    @Test
+    public void findMaxYearBook() {
+        Book book = BookFunctions.maxYearBook(bookListTest);
+        Assert.assertEquals("Szósta", book.getTitle());
+    }
+    //zad 4 stream
+    @Test
+    public void findMaxYearBookStream(){
+        Optional<Book> book= BookFunctions.maxYearBookStream(bookListTest);
+        Assert.assertEquals("Szósta", book.get().getTitle());
+    }
+}
