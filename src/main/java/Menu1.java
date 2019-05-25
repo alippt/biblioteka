ba1ea1e92b195fa58e5a4e74b11f575eec7dc9d2
Komputer Domowy <bartosz9-86@tlen.pl>
2019-05-25 10:30:58 +0200

    lokalne zmiany

--- src/main/java/Menu1.java
+++ src/main/java/Menu1.java
@@ -1,22 +1,40 @@
+import groovyjarjarantlr.StringUtils;
+import sun.util.locale.LocaleUtils;
+
 import java.io.IOException;
 import java.util.Scanner;
 
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
 public class Menu1 {
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner ( System.in );
 
+static String cos;
+    static void menu()  {
 
-    static void menu() throws IOException {
-        int option = 0;
-        System.out.println("1. Wyświetl liste ksiązek");
-        System.out.println("2. Wyjdź");
+        System.out.println ( "1. Wyświetl liste ksiązek" );
+        System.out.println ( "2. Wyjdź" );
+        cos = scanner.nextLine ();
+        if (isNumeric ( cos )) {
+            userChoice ();
+        }else {
+            System.out.println ("Wybierz liczbę 1 lub 2");
+            menu ();
+        }
+    }
+
+    static void userChoice() {
+        int option= Integer.parseInt ( cos );
         while (option != 2) {
-            option = Integer.parseInt(scanner.nextLine());
             switch (option) {
                 case 1:
-                    BookList.readBookList();
+                    BookList.readBookList ();
                     break;
                 case 2:
-                    System.out.println("Do widzenia");
+                    System.out.println ( "Do widzenia" );
+                    break;
+                default:
+                    System.out.println ( "wybierz 1 lub 2" );
                     break;
             }
         }
